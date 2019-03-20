b77fe19fd7a73812199086c78a2c7895e6b4e708
atsyb <dazam@ukr.net>
2019-03-20 18:22:40 +0200

    Exercise 1..7

--- src/home/work2/Calculation.java
+++ src/home/work2/Calculation.java
@@ -1,4 +1,106 @@
 package home.work2;
 
 public class Calculation {
+    void calculateRootsByDiscriminant(int memberA, int memberB, int memberC) {
+        double discriminant = (Math.pow(memberB, 2) - 4 * memberA * memberC);
+        System.out.println("D = " + (discriminant));
+        if (discriminant > 0) {
+            System.out.println("x1 = " + (-memberB - Math.sqrt(discriminant)) / (2 * memberA));
+            System.out.println("x2 = " + (-memberB + Math.sqrt(discriminant)) / (2 * memberA));
+        } else if (discriminant == 0) {
+            System.out.println("x = " + (-memberB / (2 * memberA)));
+        } else {
+            System.out.println("Equation has no real roots!");
+        }
+    }
+
+    String calculateRootsByVietTheorem(int memberP, int memberQ) {
+        String result = "!not picked up the roots!";
+        int root1;
+        int root2 = -Math.abs(memberP);
+        for (int x1 = -Math.abs(memberP); x1 < Math.abs(memberP) + 1; x1++) {
+            root1 = x1;
+            if (root1 * root2 == memberQ & root1 + root2 == -memberP) {
+                result = "x1 = " + root1 + "\n" + "x2 = " + root2;
+                break;
+            } else {
+                for (int x2 = -Math.abs(memberP); x2 < Math.abs(memberP) + 1; x2++) {
+                    root2 = x2;
+                    if (root1 * root2 == memberQ & root1 + root2 == -memberP) {
+                        result = "x1 = " + root1 + "\n" + "x2 = " + root2;
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    int printFibonacciSeries(int memberNumber) {
+        if (memberNumber == 0) {
+            return 0;
+        } else if (memberNumber == 1) {
+            return 1;
+        } else return printFibonacciSeries(memberNumber - 1) + printFibonacciSeries(memberNumber - 2);
+    }
+
+    void printMultiplicationTable() {
+        int i, j;
+        System.out.printf("   │");
+        for (i = 1; i < 11; i++)
+            System.out.printf("%2d ", i);
+        System.out.printf("\n───┼——————————————————\n");
+        for (i = 1; i < 11; i++) {
+            System.out.printf("%2d │", i);
+            for (j = 1; j < 11; j++)
+                System.out.printf("%2d ", i * j);
+            System.out.printf("\n");
+        }
+    }
+
+    void drawHerringbone(int count) {
+        for (int i = 0; i < count; ++i) {
+            for (int j = 0; j < i; ++j) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    void drawHerringboneBack(int count) {
+        for (int i = 0; i < count; ++i) {
+            for (int j = 0; j < count - i; ++j) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    String displayTimeOfDay(int hour) {
+        String result = "";
+        switch (hour) {
+            case 22: case 23: case 24: case 1: case 2: case 3: {
+                result = "Good night";
+                break;
+            }
+            case 4: case 5: case 6: case 7: case 8: case 9: case 10: case 11: {
+                result = "Good morning";
+                break;
+            }
+            case 12: case 13: case 14:case 15: case 16: case 17: case 18: {
+                result = "Good day";
+                break;
+            }
+            case 19: case 20: case 21: {
+                result = "Good evening";
+                break;
+            }
+            default: {
+                result = "ERROE hour!";
+            }
+        }
+        return result;
+    }
+
 }
+
--- src/home/work2/Main2.java
+++ src/home/work2/Main2.java
@@ -1,8 +1,47 @@
 package home.work2;
 
+import java.util.Scanner;
+
 public class Main2 {
-    public static void main(String[] args){
-        System.out.println("__SkillsUp:HomeWork2__");
+    public static void main(String[] args) {
+        System.out.println("__SkillsUp:HomeWork2__\n");
+        Scanner scanner = new Scanner(System.in);
+        Calculation calc = new Calculation();
+
+        System.out.println("   Exercise #1 Calculate the roots of a square trinomial of the form:\n   ax^2+bx+c=0 (ByDiscriminant)");
+        System.out.println("Enter a:");
+        int numberA = scanner.nextInt();
+        System.out.println("Enter b:");
+        int numberB = scanner.nextInt();
+        System.out.println("Enter c:");
+        int numberC = scanner.nextInt();
+        System.out.println("Result:");
+        calc.calculateRootsByDiscriminant(numberA, numberB, numberC);
+        System.out.println();
+
+        System.out.println("   Exercise #2 Calculate the roots of a square trinomial of the form:\n   x2+px+q=0 (ByVietTheorem)");
+        System.out.println(calc.calculateRootsByVietTheorem(-14,40));
+
+        System.out.println("   Exercise #3 Print a series of Fibonacci numbers");
+        for (int i = 1; i<10;i++) {
+            System.out.println(calc.printFibonacciSeries(i));
+        }
+
+        System.out.println("   Exercise #4 Print a multiplication table");
+        calc.printMultiplicationTable();
+
+        System.out.println("   Exercise #5 Draw herringbone");
+        calc.drawHerringbone(10);
+
+        System.out.println("   Exercise #6 Draw herringbone Back");
+        calc.drawHerringboneBack(10);
+
+        System.out.println("   Exercise #7 Display time of day");
+        System.out.println("how many hours is it now?");
+        int numberHour = scanner.nextInt();
+        System.out.println(calc.displayTimeOfDay(numberHour));
+
+        System.out.println("   Exercise #8");
 
     }
 }
