ac264cabdf96ac1a690e00a8508c311bcb83edb1
atsyb <dazam@ukr.net>
2019-03-20 23:15:29 +0300

    Homework number 2

--- src/home/work2/Calculation.java
+++ src/home/work2/Calculation.java
@@ -79,19 +79,39 @@ public class Calculation {
     String displayTimeOfDay(int hour) {
         String result = "";
         switch (hour) {
-            case 22: case 23: case 24: case 1: case 2: case 3: {
+            case 22:
+            case 23:
+            case 24:
+            case 1:
+            case 2:
+            case 3: {
                 result = "Good night";
                 break;
             }
-            case 4: case 5: case 6: case 7: case 8: case 9: case 10: case 11: {
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11: {
                 result = "Good morning";
                 break;
             }
-            case 12: case 13: case 14:case 15: case 16: case 17: case 18: {
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18: {
                 result = "Good day";
                 break;
             }
-            case 19: case 20: case 21: {
+            case 19:
+            case 20:
+            case 21: {
                 result = "Good evening";
                 break;
             }
@@ -100,6 +120,22 @@ public class Calculation {
             }
         }
         return result;
+    }
+
+    void PrintOddNumbers(int fromNumber, int toNumber) {
+        for (int i = fromNumber; i <= toNumber; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    void PrintEvenNumbers(int fromNumber, int toNumber) {
+        for (int i = fromNumber; i <= toNumber; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 
 }
--- src/home/work2/Main2.java
+++ src/home/work2/Main2.java
@@ -20,10 +20,10 @@ public class Main2 {
         System.out.println();
 
         System.out.println("   Exercise #2 Calculate the roots of a square trinomial of the form:\n   x2+px+q=0 (ByVietTheorem)");
-        System.out.println(calc.calculateRootsByVietTheorem(-14,40));
+        System.out.println(calc.calculateRootsByVietTheorem(-14, 40));
 
         System.out.println("   Exercise #3 Print a series of Fibonacci numbers");
-        for (int i = 1; i<10;i++) {
+        for (int i = 1; i < 10; i++) {
             System.out.println(calc.printFibonacciSeries(i));
         }
 
@@ -41,7 +41,25 @@ public class Main2 {
         int numberHour = scanner.nextInt();
         System.out.println(calc.displayTimeOfDay(numberHour));
 
-        System.out.println("   Exercise #8");
+        System.out.println("   Exercise #8 Lottery ticket");
+        {
+            int randomNumber = (int) (Math.random() * 20) + 1;
+            System.out.println("The lottery ticket dropped the number: " + randomNumber);
+            do {
+                System.out.print("Guess the number: ");
+            } while (scanner.nextInt() != randomNumber);
+            System.out.print("Victory!");
+        }
 
+        System.out.println("   Exercise #9 Odd numbers in the specified range");
+        System.out.println("Enter range from");
+        System.out.print("from: ");
+        int numberFrom = scanner.nextInt();
+        System.out.print("to: ");
+        int numberTo = scanner.nextInt();
+        calc.PrintOddNumbers(numberFrom, numberTo);
+
+        System.out.println("   Exercise #10 Even numbers in the specified range");
+        calc.PrintEvenNumbers(numberFrom, numberTo);
     }
 }
