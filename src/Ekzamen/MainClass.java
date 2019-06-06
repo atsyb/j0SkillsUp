6b1b60fea1c3ab8b3f15dcad708c1f922b2cdab6
atsyb <dazam@ukr.net>
2019-06-06 20:43:25 +0300

    Ekzamen

--- src/Ekzamen/MainClass.java
+++ src/Ekzamen/MainClass.java
@@ -3,6 +3,8 @@ package Ekzamen;
 public class MainClass {
     public static void main(String[] args){
         System.out.println("__SkillsUp:Ekzamen__");
+        Employee employee = new Employee("Sam","Microsoft");
+        employee.displiay();
 
     }
 }
---
+++ src/Ekzamen/User.java
@@ -0,0 +1,11 @@
+package Ekzamen;
+
+public class User implements Comparable<User>{
+    private String name;
+    private int age;
+
+    @Override
+    public int compareTo(User o) {
+        return 0;
+    }
+}
